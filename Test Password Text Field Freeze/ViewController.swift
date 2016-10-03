662e6c5d36c0ed3b78d996ba749d91ccb1c9c63b
sQu1rr <sQu1rr1c@gmail.com>
2016-10-03 09:46:06 +0100

    Initial Commit

---
+++ Test Password Text Field Freeze/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Test Password Text Field Freeze
+//
+//  Created by Aleksandr Belkin on 03/10/2016.
+//  Copyright © 2016 Aleksandr Belkin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
